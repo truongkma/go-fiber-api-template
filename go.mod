b1010b098bd205172520efff1e8a897b2fafda64
behemoth-truongnd <truongnd@behemoth.vn>
2022-12-29 11:33:06 +0700

    add jwt middleware

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/gofiber/jwt/v2 v2.2.7 // indirect
+	github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/joho/godotenv/autoload" // load .env file automatically
 	"github.com/truongkma/go-fiber-api-template/pkg/configs"
+	"github.com/truongkma/go-fiber-api-template/pkg/middleware"
 	"github.com/truongkma/go-fiber-api-template/pkg/utils"
 	"os"
 )
@@ -12,6 +13,9 @@ func main() {
 	// Define Fiber config.
 	config := configs.FiberConfig()
 	app := fiber.New(config)
+
+	// Middlewares.
+	middleware.FiberMiddleware(app) // Register Fiber's middleware for app.
 
 	// Start server (with or without graceful shutdown).
 	if os.Getenv("STAGE_STATUS") == "dev" {
