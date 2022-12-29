f5f7e1ae02b59ca40d2b732c1e7c99b20840da82
behemoth-truongnd <truongnd@behemoth.vn>
2022-12-29 13:38:29 +0700

    add editor config

--- platform/migrations/000001_create_init_tables.up.sql
+++ platform/migrations/000001_create_init_tables.up.sql
@@ -6,13 +6,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 SET TIMEZONE = "Asia/Ho_Chi_Minh";
 -- Create users table
 CREATE TABLE users (
-    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
-    updated_at TIMESTAMP NULL,
-    email VARCHAR (255) NOT NULL UNIQUE,
-    password_hash VARCHAR (255) NOT NULL,
-    user_status INT NOT NULL,
-    user_role VARCHAR (25) NOT NULL
+  id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+  updated_at TIMESTAMP NULL,
+  email VARCHAR (255) NOT NULL UNIQUE,
+  password_hash VARCHAR (255) NOT NULL,
+  user_status INT NOT NULL,
+  user_role VARCHAR (25) NOT NULL
 );
 -- Add indexes
 CREATE INDEX active_users ON users (id)
