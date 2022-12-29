ea98b43f67fe59cc2f7231ec118083628886ce42
behemoth-truongnd <truongnd@behemoth.vn>
2022-12-29 11:28:38 +0700

    add main go file

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/joho/godotenv/autoload" // load .env file automatically
+	"github.com/truongkma/go-fiber-api-template/pkg/configs"
+	"github.com/truongkma/go-fiber-api-template/pkg/utils"
+	"os"
+)
+
+func main() {
+	// Define Fiber config.
+	config := configs.FiberConfig()
+	app := fiber.New(config)
+
+	// Start server (with or without graceful shutdown).
+	if os.Getenv("STAGE_STATUS") == "dev" {
+		utils.StartServer(app)
+	} else {
+		utils.StartServerWithGracefulShutdown(app)
+	}
+}
